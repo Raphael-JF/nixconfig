a9c63274414f2f4a0e1737410d7dc6e00aa7d3d5
Raphaël Jontef <raphael.jontef@bordeaux-inp.fr>
2026-04-20 19:39:37 +0200

    ho

--- home/home.nix
+++ home/home.nix
@@ -2,8 +2,13 @@
 
 
 {
+
     
 home-manager.users.raph = {
+    imports = [
+        ./gnome.nix
+    ];
+
     home.username = "raph";
     home.homeDirectory = "/home/raph";
 
@@ -18,51 +23,7 @@ home-manager.users.raph = {
         platformio
     ];
 
-    dconf.settings = {
-        "org/gnome/system/locale" = {
-            region = "fr_FR.UTF-8";
-        };
-        "org/gnome/settings-daemon/plugins/media-keys" = {
-            custom-keybindings = [
-                "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
-            ];  
-        };
-        "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
-            name = "Terminal";
-            command = "kgx";
-            binding = "<Ctrl><Alt>T";
-        };
-
-        "org/gnome/desktop/wm/keybindings" = {
-            move-to-workspace-left=["<Shift><Alt>x"];
-            move-to-workspace-right=["<Shift><Alt>c"];
-            switch-to-workspace-right = [ "<Alt>c" ];
-            switch-to-workspace-left  = [ "<Alt>x" ];
-        };
-        "org/gnome/desktop/peripherals/touchpad" = {
-            click-method = "areas";
-            two-finger-scrolling-enabled	= true;	
-        };  
-        "org/gnome/settings-daemon/plugins/color" = {
-            night-light-enabled = true;
-            night-light-schedule-automatic = false;
-            night-light-schedule-from = 0.0;
-            night-light-schedule-to = 0.0;
-            night-light-temperature = lib.gvariant.mkUint32 3526;
-        };
-        "org/gnome/shell/app-switcher" = {
-            current-workspace-only=true;
-
-        };
-
-        "org/gnome/desktop/wm/preferences" = {
-            num-workspaces = 2;
-            
-        };
-        "org/gnome/mutter" = { 
-            dynamic-workspaces = false;
-        };
-    };
+    
 
     # wayland.windowManager.hyprland = {
     #     settings = import ./hyprland/hyprland.nix;
