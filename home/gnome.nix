33f17cea6052bac12792c5987033431846d3f9fc
RJ root laptop <raphaeljontef@hotmail.com>
2026-04-20 20:58:25 +0200

    fgjzg

---
+++ home/gnome.nix
@@ -0,0 +1,62 @@
+{ config, pkgs, lib, ... }:
+
+{
+  dconf.settings = {
+
+    # --- Custom keybindings ---
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      custom-keybindings = [
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+      ];
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+      name = "Terminal";
+      command = "kgx";
+      binding = "<Ctrl><Alt>T";
+    };
+
+    # --- Window manager ---
+    "org/gnome/desktop/wm/keybindings" = {
+      move-to-workspace-left  = ["<Shift><Alt>x"];
+      move-to-workspace-right = ["<Shift><Alt>c"];
+      switch-to-workspace-right = ["<Alt>c"];
+      switch-to-workspace-left  = ["<Alt>x"];
+
+      switch-group = ["<Alt>Tab"];
+      switch-group-backward = ["<Shift><Alt>Tab"];
+
+      switch-applications = ["<Alt>Tab"];
+      switch-applications-backward = ["<Shift><Alt>Tab"];
+    };
+
+    # --- Touchpad ---
+    "org/gnome/desktop/peripherals/touchpad" = {
+      click-method = "areas";
+      two-finger-scrolling-enabled = true;
+    };
+
+    # --- Night light ---
+    "org/gnome/settings-daemon/plugins/color" = {
+      night-light-enabled = true;
+      night-light-schedule-automatic = false;
+      night-light-schedule-from = 0.0;
+      night-light-schedule-to = 0.0;
+      night-light-temperature = lib.gvariant.mkUint32 3526;
+    };
+
+    # --- App switcher ---
+    "org/gnome/shell/app-switcher" = {
+      current-workspace-only = true;
+    };
+
+    # --- Workspaces ---
+    "org/gnome/desktop/wm/preferences" = {
+      num-workspaces = 2;
+    };
+
+    "org/gnome/mutter" = {
+      dynamic-workspaces = false;
+    };
+  };
+}
--- home/home.nix
+++ home/home.nix
@@ -20,8 +20,57 @@ home-manager.users.raph = {
         # wofi
         # waybar
         firefox
-        platformio
     ];
+
+programs.vscode = {
+    enable = true;
+    package = pkgs.vscodium;
+
+    profiles.premier = {
+        enableExtensionUpdateCheck = false;
+        enableMcpIntegration = false;
+        enableUpdateCheck = false;
+
+        # The extensions Visual Studio Code should be started with
+        extensions = with pkgs.vscode-extensions; [
+
+        # --- Nix ---
+        bbenoist.nix
+
+        # --- C / C++ ---
+        ms-vscode.cpptools
+        ms-vscode.cpptools-extension-pack
+        ms-vscode.cmake-tools
+        ms-vscode.cpp-devtools
+
+        # --- Python ---
+        ms-python.python
+        ms-python.vscode-pylance
+
+        # --- Debug JS (souvent utile indirectement) ---
+        ms-vscode.js-debug
+
+        # --- UI / UX ---
+        usernamehw.errorlens
+
+        ];
+        # Defines global user snippets
+        globalSnippets = null;
+        # Keybindings written to Visual Studio Code's keybindings.json
+        keybindings = builtins.fromJSON (builtins.readFile ./vscode/keybindings.json);
+        # Defines user snippets for different languages
+        languageSnippets = {
+            c = builtins.fromJSON (builtins.readFile ./vscode/snippets/c.json);
+            latex = builtins.fromJSON (builtins.readFile ./vscode/snippets/latex.json);
+        };
+        # Configuration written to Visual Studio Code's mcp.json
+        userMcp = null;
+        # Configuration written to Visual Studio Code's settings.json
+        userSettings = builtins.fromJSON (builtins.readFile ./vscode/settings.json);
+        # Configuration written to Visual Studio Code's tasks.json
+        userTasks = null;
+    };
+};
 
     
 
