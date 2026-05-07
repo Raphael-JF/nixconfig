1d683fb6152a2bb71a6fa1b2eb0a149474eed7d7
Raphaël Jontef <raphael.jontef@enseirb-matmeca.fr>
2026-05-07 12:51:48 +0200

    a

--- home/home.nix
+++ home/home.nix
@@ -1,10 +1,11 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 {
     home-manager.extraSpecialArgs = {
         raph = {
             hostType = config.raph.hostType;
         };
+        inherit pkgs;
     };
 
     home-manager.users.raph = import ./home-manager.nix;
