4e847486fba8d82e9fd2e406b7ed30f0ee9dfab6
Votre Nom <Vous@exemple.com>
2026-07-17 22:46:46 +0200

    service

---
+++ server/modules/services/public-ip.nix
@@ -0,0 +1,68 @@
+{
+  pkgs,
+  config,
+  ...
+}:
+
+let
+  script = pkgs.writeShellApplication {
+    name = "update-public-ip";
+
+    runtimeInputs = with pkgs; [
+      curl
+      git
+      jq
+    ];
+
+    text = ''
+      set -euo pipefail
+
+      REPO=/var/lib/public-ip
+      FILE=$REPO/ip.txt
+
+      cd "$REPO"
+
+      NEW_IP=$(curl -fsSL https://api.ipify.org)
+
+      OLD_IP=""
+
+      if [ -f "$FILE" ]; then
+          OLD_IP=$(cat "$FILE")
+      fi
+
+      if [ "$NEW_IP" = "$OLD_IP" ]; then
+          exit 0
+      fi
+
+      echo "$NEW_IP" > "$FILE"
+
+      git add ip.txt
+      git commit -m "Update IP: $NEW_IP"
+      git push
+    '';
+  };
+in
+{
+
+  systemd.services.public-ip = {
+    description = "Update public IP on GitHub";
+
+    serviceConfig = {
+      Type = "oneshot";
+      User = "root";
+    };
+
+    script = "${script}/bin/update-public-ip";
+  };
+
+  systemd.timers.public-ip = {
+    wantedBy = [ "timers.target" ];
+
+    timerConfig = {
+      OnBootSec = "2min";
+      OnUnitActiveSec = "15min";
+      Unit = "public-ip.service";
+    };
+  };
+
+}
