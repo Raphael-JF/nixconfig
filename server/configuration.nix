9e510fd5b13aa7c68d5fcab0fc82d968c697ad84
Raphaël Jontef <raphael.jontef@enseirb-matmeca.fr>
2026-07-17 04:44:42 +0200

    aezga

--- server/configuration.nix
+++ server/configuration.nix
@@ -10,14 +10,17 @@
     (modulesPath + "/profiles/qemu-guest.nix")
     ./disk-config.nix
   ];
-  boot.loader.grub = {
-    # no need to set devices, disko will add all devices that have a EF02 partition to the list already
-    # devices = [ ];
-    efiSupport = true;
-    efiInstallAsRemovable = true;
-  };
+  
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
   services.openssh.enable = true;
 
+  users.users.raph = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    initialPassword = "a";
+ };
   environment.systemPackages = map lib.lowPrio [
     pkgs.curl
     pkgs.gitMinimal
