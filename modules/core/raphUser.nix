2101f39d6c21ef23e2f21c018934da37c99d0c17
Raphaël Jontef <raphael.jontef@enseirb-matmeca.fr>
2026-08-01 13:26:08 +0200

    secrets

--- modules/core/raphUser.nix
+++ modules/core/raphUser.nix
@@ -1,10 +1,10 @@
 
-{ ... }:
+{ config, ... }:
 
 {
   users.users.raph = {
-    initialPassword = "";
     isNormalUser = true;
+    hashedPasswordFile = config.sops.secrets.raphPassword.path;
 
     extraGroups = [
       "wheel"
@@ -12,9 +12,10 @@
     ];
 
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE+mpQTHhucCVywM74pja3n+oLvEbN3Rh6Kdr0ogjjy2 raph@raph-desktop"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIESTCleHAfmgUQfFz0Rp3xtKmgDzowuKmRiwE6m6H6RA raph@raph-laptop"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMjPNDWi4vjiqAp+MdByvlIEhKsr/TDOLQVGq4UQSiEm mobile@iPhone"
+      builtins.readFile config.sops.secrets.desktop.public.path
+      builtins.readFile config.sops.secrets.laptop.public.path
+      builtins.readFile config.sops.secrets.server.public.path
+      builtins.readFile config.sops.secrets.iphone.public.path
     ];
 
   };
--- modules/core/security.nix
+++ modules/core/security.nix
@@ -1,4 +1,16 @@
-{ pkgs, hostname,  ... }:
+{ pkgs, hostname, config, lib, ... }:
+let
+  hosts = [ "laptop" "desktop" "server" "iphone" ];
+
+  mkPublicKey = host: {
+    mode = "644";
+    owner = config.users.users.${hostname}.name;
+    group = config.users.users.${hostname}.group;
+    path = lib.mkIf (host == hostname)
+      "/home/${config.users.users.${hostname}.name}/.ssh/${host}.pub";
+  };
+in
+
 {
   security.sudo.wheelNeedsPassword = true;
   services.fprintd.enable = true; # Enable fingerprint authentication
@@ -8,8 +20,22 @@
     age
   ];
 
-    sops.defaultSopsFile = ./secrets/secrets.yaml;
-    sops.defaultSopsFormat= "yaml";
-    
-    sops.age.keyFile = "/home/${hostname}/.config/sops/age/keys.txt";
+  sops.defaultSopsFile = ./secrets/secrets.yaml;
+  sops.defaultSopsFormat= "yaml";
+  
+  sops.age.keyFile = "/home/${hostname}/.config/sops/age/keys.txt";
+
+  sops.secrets =
+    lib.genAttrs
+      (map (h: "ssh/${h}/public") hosts)
+      (name: mkPublicKey (builtins.elemAt (lib.splitString "/" name) 1))
+    //
+    {
+      "ssh/${hostname}/private" = {
+        mode = "600";
+        owner = config.users.users.${hostname}.name;
+        group = config.users.users.${hostname}.group;
+        path = "/home/${hostname}/.ssh/${hostname}";
+      };
+    };
 }
