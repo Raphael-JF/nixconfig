f54b2c93acc1d69aef1148d6092d9b89d9cf3a47
Raphaël Jontef <raphael.jontef@enseirb-matmeca.fr>
2026-08-10 15:18:43 +0200

    progrès

--- modules/core/raphUser.nix
+++ modules/core/raphUser.nix
@@ -15,6 +15,7 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMjPNDWi4vjiqAp+MdByvlIEhKsr/TDOLQVGq4UQSiEm mobile@iPhone"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE+mpQTHhucCVywM74pja3n+oLvEbN3Rh6Kdr0ogjjy2 raph@raph-desktop"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIESTCleHAfmgUQfFz0Rp3xtKmgDzowuKmRiwE6m6H6RA raph@raph-laptop"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPaba6PCu89ZE4P3XwwoPtv/ngl6druZ+oswSOphVHxJ raph@raph-server"
     ];
   };
 
