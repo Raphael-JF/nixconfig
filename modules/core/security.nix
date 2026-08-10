403e8b1f49ef5625be369737fa1a837e542e0ea0
Raphaël Jontef <raphael.jontef@enseirb-matmeca.fr>
2026-08-10 17:42:07 +0200

    hop

--- modules/core/security.nix
+++ modules/core/security.nix
@@ -20,7 +20,7 @@ in
     age
   ];
 
-  sops.defaultSopsFile = ../../secrets.yaml;
+  sops.defaultSopsFile = ../../secrets/secrets.yaml;
   sops.defaultSopsFormat= "yaml";
   
   # sops.age.keyFile = "/home/raph/.config/sops/age/keys.txt";
