3e9314247dddda12030e380469f6939ba86ccf01
Raphaël Jontef <raphael.jontef@enseirb-matmeca.fr>
2026-08-10 17:59:21 +0200

    rzghz

--- modules/services/publicIp/users.nix
+++ modules/services/publicIp/users.nix
@@ -3,16 +3,15 @@
 {
   config = {
     users.groups.publicIpUser = {}; 
-    users.users.publicIpUser = {
-                   isSystemUser = true;
-              group = "publicIpUser";
-              home = "/var/lib/publicIpUser";
-              createHome = true;
-            homeMode = "770";             
-            } 
-    //
-    {
-      raph.extraGroups = [ "publicIpUser" ];
+    users.users = {
+        publicIpUser = {
+          isSystemUser = true;
+          group = "publicIpUser";
+          home = "/var/lib/publicIpUser";
+          createHome = true;
+          homeMode = "770";             
+        };
+        raph.extraGroups = [ "publicIpUser" ];
     };
   };
 }
