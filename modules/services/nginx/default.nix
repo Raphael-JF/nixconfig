c10f2cbbe4ee9126ecd6e31600d8e165fc163275
Raphaël Jontef <raphael.jontef@enseirb-matmeca.fr>
2026-08-15 22:34:48 +0200

    zou

--- modules/services/nginx/default.nix
+++ modules/services/nginx/default.nix
@@ -5,17 +5,33 @@
     enable = true;
 
     virtualHosts."_" = {
-      listen = [
-        {
-          addr = "0.0.0.0";
-          port = 80;
-        }
-      ];
-      root = pkgs.writeTextDir "index.html" (builtins.readFile ./index.html);
+      listen = [{
+        addr = "0.0.0.0";
+        port = 80;
+      }];
+
+      extraConfig = ''
+        return 301 https://$host$request_uri;
+      '';
+    };
+
+    virtualHosts."https" = {
+      listen = [{
+        addr = "0.0.0.0";
+        port = 443;
+        ssl = true;
+      }];
+
+      sslCertificate = "/var/lib/nginx/certs/server.crt";
+      sslCertificateKey = "/var/lib/nginx/certs/server.key";
+
+      root = pkgs.writeTextDir "index.html"
+        (builtins.readFile ./index.html);
     };
   };
 
   networking.firewall.allowedTCPPorts = [
     80
+    443
   ];
 }
