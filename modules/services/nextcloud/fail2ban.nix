de1405940a6bdc191384f067b25b7026616d2d98
Raphaël Jontef <raphael.jontef@enseirb-matmeca.fr>
2026-08-16 02:55:11 +0200

    go

--- modules/services/nextcloud/fail2ban.nix
+++ modules/services/nextcloud/fail2ban.nix
@@ -1,27 +1,33 @@
-# fail2ban.nix
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 {
   services.fail2ban = {
     enable = true;
+    # The jail file defines how to handle the failed authentication attempts found by the Nextcloud filter
     # Ref: https://docs.nextcloud.com/server/latest/admin_manual/installation/harden_server.html#setup-a-filter-and-a-jail-for-nextcloud
     jails = {
       nextcloud.settings = {
+        # START modification to work with syslog instead of logile
         backend = "systemd";
         journalmatch = "SYSLOG_IDENTIFIER=Nextcloud";
+        # END modification to work with syslog instead of logile
         enabled = true;
-        port = "http,https";
+        port = 443;
+        protocol = "tcp";
         filter = "nextcloud";
-        maxretry = 4;
-        bantime = 86401;
-        findtime = 43201;
+        maxretry = 3;
+        bantime = 86400;
+        findtime = 43200;
       };
     };
   };
 
-  environment.etc."fail2ban/filter.d/nextcloud.local".text = lib.mkDefault ''
-    [Definition]
-    failregex = ^.*"remoteAddr":"<HOST>".*"message":"Login failed:
-                ^.*"remoteAddr":"<HOST>".*"message":"Two-factor challenge failed:
-                ^.*"remoteAddr":"<HOST>".*"message":"Trusted domain error.
-  '';
+  environment.etc = {
+    # Adapted failregex for syslogs
+    "fail2ban/filter.d/nextcloud.local".text = pkgs.lib.mkDefault (pkgs.lib.mkAfter ''
+      [Definition]
+      failregex = ^.*"remoteAddr":"&lt;HOST&gt;".*"message":"Login failed:
+                  ^.*"remoteAddr":"&lt;HOST&gt;".*"message":"Two-factor challenge failed:
+                  ^.*"remoteAddr":"&lt;HOST&gt;".*"message":"Trusted domain error.
+    '');
+  };
 }
