00a8eea6003ab68e608ae6400a9eb55148ab3915
Raphaël Jontef <raphael.jontef@enseirb-matmeca.fr>
2026-08-21 23:28:27 +0200

    hop

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -10,32 +10,34 @@
 
     ../../modules/services/sshServer.nix
   ];
+  config = {
+    services.sshServer.enable = true;
 
-  config.services.sshServer.enable = true;
+    packages.development.enable = true; 
+    packages.gaming.enable = true;
 
-  packages.development.enable = true; 
-  packages.gaming.enable = true;
+    #
+    # ===== NVIDIA GRAPHICS =====
+    hardware.graphics.enable = true;
+    services.xserver.videoDrivers = [ "nvidia" ];
+    hardware.nvidia.open = false;  # see the note above
+    hardware.nvidia.modesetting.enable = true; 
+      hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "580.159.03";
+      sha256_64bit = "sha256-MshdmbD2QMlQH2GzndrSCP0CiNAVxPvF/QQ1wHeD+nc=";
+      sha256_aarch64 = "sha256-MshdmbD2QMlQH2GzndrSCP0CiNAVxPvF/QQ1wHeD+nc=";
+      openSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
+      settingsSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
+      persistencedSha256 = lib.fakeSha256;
+    };
+    hardware.nvidia.powerManagement.enable = true;
 
-  #
-  # ===== NVIDIA GRAPHICS =====
-  hardware.graphics.enable = true;
-  services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.nvidia.open = false;  # see the note above
-  hardware.nvidia.modesetting.enable = true; 
-    hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-    version = "580.159.03";
-    sha256_64bit = "sha256-MshdmbD2QMlQH2GzndrSCP0CiNAVxPvF/QQ1wHeD+nc=";
-    sha256_aarch64 = "sha256-MshdmbD2QMlQH2GzndrSCP0CiNAVxPvF/QQ1wHeD+nc=";
-    openSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
-    settingsSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
-    persistencedSha256 = lib.fakeSha256;
+
+    
+
+
+
+    system.stateVersion = "26.05";
+
   };
-  hardware.nvidia.powerManagement.enable = true;
-
-
-  
-
-
-
-  system.stateVersion = "26.05";
 }
