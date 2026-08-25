62538049e837fe71f4885e5aafee08820eccb414
Raphaël Jontef <raphael.jontef@enseirb-matmeca.fr>
2026-08-25 03:48:43 +0200

    grzg

--- modules/core/boot.nix
+++ modules/core/boot.nix
@@ -8,11 +8,4 @@
 
   boot.tmp.cleanOnBoot = true;
 
-
-  boot.initrd.systemd = {
-    enable = true;
-    services."dev-pool-root.device".unitConfig = {
-      JobTimeoutSec = "infinity";
-    };
-  };
 }
