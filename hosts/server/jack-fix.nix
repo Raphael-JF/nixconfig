ad9e4132bf40a9874500056ea14806f85c8f79b9
Raphaël Jontef <raphael.jontef@enseirb-matmeca.fr>
2026-08-28 17:13:23 +0200

    grezg

--- hosts/server/jack-fix.nix
+++ hosts/server/jack-fix.nix
@@ -2,17 +2,21 @@
 { pkgs, ... }:
 
 let
-  hda-jack-fix = pkgs.writeText "hda-jack-fix.fw" ''
+  hda-jack-fix = pkgs.runCommand "hda-jack-fix" {} ''
+    mkdir -p $out/lib/firmware
+    cat > $out/lib/firmware/hda-jack-fix.fw <<'EOF'
     [codec]
     0x10ec0269 0x27821404 0
 
     [hint]
     jack_detect = no
+    EOF
   '';
 in
 {
-  environment.systemPackages = [ pkgs.pulseaudio ];
-  hardware.firmware = [ hda-jack-fix ];
+  hardware.firmware = [
+    hda-jack-fix
+  ];
 
   boot.extraModprobeConfig = ''
     options snd-hda-intel patch=hda-jack-fix.fw
