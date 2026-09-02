b907533a1f9e46be6faa75ddb5bbde0ae9e9f01f
Raphaël Jontef <raphael.jontef@enseirb-matmeca.fr>
2026-09-02 15:13:30 +0200

    ezifzefg

--- modules/services/backup.nix
+++ modules/services/backup.nix
@@ -101,8 +101,8 @@ in
     ];
 
     environment.etc."btrbk/btrbk.conf".text = ''
-      snapshot_preserve_min 2d
-      snapshot_preserve 14d
+      snapshot_preserve_min 0d
+      snapshot_preserve 0d
 
       target_preserve_min 2d
       target_preserve 1m
