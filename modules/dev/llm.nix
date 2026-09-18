0b6a06597ee62171e5ffa8401364417763ab8c16
Raphaël Jontef <raphael.jontef@enseirb-matmeca.fr>
2026-09-18 16:59:18 +0200

    llm

--- modules/dev/llm.nix
+++ modules/dev/llm.nix
@@ -4,5 +4,29 @@
       opencode
       pkgs.writeShellScriptBin "llm" lib.readFile ./llm.sh
     ];
+    environment.etc."opencode.json".text = ''
+{
+	"$schema":"https://opencode.ai/config.json",
+	"provider":{
+		"lmstudio":{
+			"npm":"@ai-sdk/openai-compatible",
+			"name":"LM Studio (local)",
+			"options":{
+				"baseURL":"http://127.0.0.1:1234/v1"
+			},
+			"models":{
+					"ministral-3-14b-instruct-2512": {  
+					"name":"Ministral 3",
+				      "options":{"temperature":0.1}	
+				}
+			}
+		}
+	}
+}
+    '';
+
+    systemd.tmpfiles.rules = [
+      "L+ /home/raph/.config/opencode/opencode.json - - - - /etc/opencode.json"
+    ];
   };
 }
