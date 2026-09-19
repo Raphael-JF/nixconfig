ffe9d2c9433266ca38d2ffebe2b1052435ce629a
Raphaël Jontef <raphael.jontef@enseirb-matmeca.fr>
2026-09-19 02:59:20 +0200

    backup postgre

--- modules/services/backupPostgresql.nix
+++ modules/services/backupPostgresql.nix
@@ -9,13 +9,18 @@
     serviceConfig = {
       Type = "oneshot";
       User = "postgres";
+      UMask = "0077";
     };
 
     script = ''
       ${pkgs.coreutils}/bin/mkdir -p /data/backupPostgresql
+      ${pkgs.coreutils}/bin/chown postgres:postgres /data/backupPostgresql
+      ${pkgs.coreutils}/bin/chmod 700 /data/backupPostgresql
 
       ${pkgs.postgresql_18}/bin/pg_dumpall \
         --file=/data/backupPostgresql/all.sql
+
+      ${pkgs.coreutils}/bin/chmod 600 /data/backupPostgresql/all.sql
     '';
   };
 
